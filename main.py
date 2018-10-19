ae4cec8b1f02cebe69c1100fa8c6cbc5d49a2dd7
Zoom124 <gigzim345@gmail.com>
2018-10-19 12:33:54 +0300

    NikitaP

--- main.py
+++ main.py
@@ -22,13 +22,23 @@ while j < n-1:
 print(array)
 '''
 def main(*args):
-  for c in args:
+  l = list(args)
+  i = 0
+  for c in l:
+    #print('l[{}] = {}'.format(i,type(c)))
     if type(c) == str:
-      return "Неверный формат, ожидался Int, введен Str"
-  return sorted(args)
+      if (not c.isdigit() and not c.isalpha()) or c.isalpha():
+        return "Ошибка! Неверный формат ввода {}".format(c)
+      elif c.isdigit():
+        l[i] = int(c)
+      else:
+        l[i] = int(ord(c))
+    i+=1
+  return sorted(l)
 
 if __name__ == '__main__':
   assert main(23,0,2,1,44,4) == [0,1,2,4,23,44],"Сортировка прошла не удачно"
   assert main(1,0,2,3,4,5) == [0,1,2,3,4,5],"Сортировка прошла не удачно"
   assert main(10,0,20,30,50,40) == [0,10,20,30,40,50],"Сортировка прошла не удачно"
-print(main(23,0,2,1,44,4))
+
+print(main(23,'dasga',0.5,True,False))
